a851ae60640b22b0a5523e02e3317c1245c8f509
alidjanoff <alidjanoff@hotmail.com>
2023-08-17 10:53:36 +0400

    Test json-server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = async () => {
+    await fetch("http://localhost:4000/products")
+      .then((res) => res.json())
+      .then((allData) => setData(allData));
+  };
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h4>{item.title}</h4>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
